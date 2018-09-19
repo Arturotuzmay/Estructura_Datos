2a33d7e7feeac5c89b4bc613e26ff707ef8a16b4
Arturo Tuz May <manuel314159265@gmail.com>
2018-09-19 09:09:34 -0500

    Ejercicio Intro a C

---
+++ tema1/ejercicio1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int a=10;
+    int b = ;
+    int c = a + b;
+    cout << c;
+    return 0;
+}
